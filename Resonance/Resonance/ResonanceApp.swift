e8b65c9d667f0f0eb107a9bb146b7b3a5d2c0950
Sepehr-samarzadeh <ssamarzadeh@gmail.com>
2026-04-07 21:39:55 +0200

    RES-0:initial commit-setting up the project

---
+++ Resonance/Resonance/ResonanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResonanceApp.swift
+//  Resonance
+//
+//  Created by Sepehr on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ResonanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
