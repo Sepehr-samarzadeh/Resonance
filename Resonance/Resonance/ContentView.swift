00f019396b8409f15a5e0deaa59480a1f5e050d9
Sepehr-samarzadeh <ssamarzadeh@gmail.com>
2026-04-09 00:05:40 +0200

    RES-4:API and developer token has been tested successfully

--- Resonance/Resonance/ContentView.swift
+++ Resonance/Resonance/ContentView.swift
@@ -2,9 +2,72 @@
 //  Created by Sepehr on 07/04/2026.
 
 import SwiftUI
+import MusicKit
 
 struct ContentView: View {
+    
     var body: some View {
+        VStack{
+            
+            makeBtn(btnText: "testResponse", color: .green)
+                
+            
+            Button{
+                print("button tapped")
+                Task {
+                    await checkAuthorization()
+                }
+            }label: {
+                Text("check authorization")
+                    .foregroundStyle(Color.white)
+                    .padding(10)
+                    .background(Color.red)
+                    .cornerRadius(10)
+            }
+        }
+    }
+    
+    func checkAuthorization() async {
+        let status = await MusicAuthorization.request()
+        
+        switch status {
+        case .authorized:
+            print("Authorized")
+            
+        case .denied:
+            print("Denied")
+            
+        case .restricted:
+            print("Restricted")
+            
+        case .notDetermined:
+            print("Not determined")
+            
+        @unknown default:
+            print("Unknown state")
+        }
+    }
+    
+    func fetchMyData() async throws {
+        let url = URL(string: "https://api.music.apple.com/v1/me/library/albums")!
+        let urlRequest = URLRequest(url: url)
+        
+        let request = MusicDataRequest(urlRequest: urlRequest)
+        let response = try await request.response()
+        
+        print(response.description) 
+    }
+    func makeBtn(btnText: String,color: Color) -> some View {
+        Button{
+            Task {try await fetchMyData()}
+        }label: {
+            Text(btnText)
+                .foregroundStyle(.white)
+                .padding(10)
+                .background(color)
+                .cornerRadius(10)
+        }
+        
     }
 }
 
